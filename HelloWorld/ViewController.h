c42f9f1199d992548eb932d05ebf5a9c8a735f09
Anson Ng <cwnga@yahoo-inc.com>
2014-11-29 18:50:32 +0800

    My initial commit message

---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Anson Ng on 11/29/14.
+//  Copyright (c) 2014 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@end
+
